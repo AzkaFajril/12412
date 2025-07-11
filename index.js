ec01e64a076e2dbc7cdefbe2cb5766a62c5debd8
AzkaFajril <123042025+AzkaFajril@users.noreply.github.com>
2025-07-12 04:00:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import userRoutes from "./api/routes/userRoutes.js";
-import cartRoutes from "./api/routes/cartRoutes.js";
-import menuRoutes from "./api/routes/menuRoutes.js";
-
-dotenv.config();
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 6001;
+const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Koneksi ke MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.error('MongoDB connection error:', err));
+//connect to mongoDB config
+mongoose.connect(`mongodb+srv://azka0012:azka0012@cluster0.zkzipyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => console.log('MongoDB connected...'))
 
-// Routes
-app.use("/users", userRoutes);
-app.use("/carts", cartRoutes);
-app.use("/menu", menuRoutes);
+  // jwt authentication
+  app.post('/jwt', async(req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '1hr'
+    })
+    res.send({token});
+  })
+
+//   import routes here
+const menuRoutes = require('./api/routes/menuRoutes');
+const cartRoutes = require('./api/routes/cartRoutes');
+const userRoutes = require('./api/routes/userRoutes')
+const paymentRoutes = require('./api/routes/paymentRoutes')
+const bookingRoutes = require("./api/routes/bookingRoutes");
+const timeSettingRoutes = require('./api/routes/timeSettingRoutes');
+app.use('/menu', menuRoutes)
+app.use('/carts', cartRoutes);
+app.use('/users', userRoutes);
+app.use('/payments', paymentRoutes);
+app.use("/bookings", bookingRoutes);
+app.use('/api/time-setting', timeSettingRoutes);
+
+
+//stripe payment route
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const amount = price * 100;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "sgd",
+
+    payment_method_types: [
+      "card"
+    ],
+
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello Foodi Client Server!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
